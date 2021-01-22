5e37c4d735d90895bf660f193c5b4f3b65630bec
Omar Danga <omardanga@yahoo.com>
2021-01-22 17:51:39 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Omar Danga</h3>
-      </header>
-    </div>
-  );
+  return <h2>Deployed</h1> ;
 }
 
 export default App;
